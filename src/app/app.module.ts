5a257cd94d5cf2089a270e4b2ef3111497d99caf
eddzmaciel <eddz-maciel@hotmail.com>
2017-08-31 19:31:23 -0500

    App Example Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//agregando el modulo pra el uso de mapas
+import {AgmCoreModule}from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MymapComponent } from './mymap/mymap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MymapComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyBq5eMU9nkbYGR6nWEkkdH92zuSZCvROL8'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mymap/mymap.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mymap',
+  templateUrl: './mymap.component.html',
+  styleUrls: ['./mymap.component.css']
+})
+export class MymapComponent implements OnInit {
+	lat=55;
+	lng=26;
+	zoom=7;
+	
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
